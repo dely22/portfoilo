93d4ba0a32d562b54791a48309d52c55481dfd36
dely22 <almktaryh@gmail.com>
2022-01-13 02:51:56 -0800

    +portfolio section

---
+++ js/app.js
@@ -0,0 +1,16 @@
+//   open nav in apps screen
+const navTogglerBtn = document.querySelector(".nav-toggler"),
+aside = document.querySelector(".aside");
+navTogglerBtn.addEventListener("click", () => 
+{
+    asideSectionTogglerBtn();
+})
+function asideSectionTogglerBtn()
+{
+    aside.classList.toggle("open");
+    navTogglerBtn.classList.toggle("open");
+    for(let i=0; i<totalSection; i++ )
+    {
+        allSection[i].classList.toggle("open");
+    }
+}
